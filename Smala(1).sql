ef6c99f259aabe6215fe7a196251da2687c5f1e8
Dabaz420 <73653156+Dabaz420@users.noreply.github.com>
2021-03-01 10:00:32 +0100

    Update Smala(1).sql

--- Smala(1).sql
+++ Smala(1).sql
@@ -8,18 +8,6 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 CREATE DATABASE `Smala` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `Smala`;
 
-CREATE TABLE `Smala_assoc_users_images` (
-  `assoc_id` int NOT NULL AUTO_INCREMENT,
-  `assoc_users_id` int NOT NULL,
-  `assoc_images_id` int NOT NULL,
-  PRIMARY KEY (`assoc_id`),
-  KEY `assoc_users_id` (`assoc_users_id`),
-  KEY `assoc_images_id` (`assoc_images_id`),
-  CONSTRAINT `Smala_assoc_users_images_ibfk_1` FOREIGN KEY (`assoc_users_id`) REFERENCES `Smala_users` (`user_id`),
-  CONSTRAINT `Smala_assoc_users_images_ibfk_2` FOREIGN KEY (`assoc_images_id`) REFERENCES `Smala_images` (`image_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-
 CREATE TABLE `Smala_images` (
   `image_id` int NOT NULL AUTO_INCREMENT,
   `image_chemin` varchar(255) COLLATE utf8_bin NOT NULL,
@@ -38,5 +26,16 @@ CREATE TABLE `Smala_users` (
 
 INSERT INTO `Smala_users` (`user_id`, `user_email`, `user_password`, `user_role`) VALUES
 (1,	'admin',	'$2y$10$xGXLLXzDd9qf6CvV0VFrhOUjajW09nqEA0ZfLOozQfNEudM.KtATK',	'admin');
+
+CREATE TABLE `Smala_assoc_users_images` (
+  `assoc_id` int NOT NULL AUTO_INCREMENT,
+  `assoc_users_id` int NOT NULL,
+  `assoc_images_id` int NOT NULL,
+  PRIMARY KEY (`assoc_id`),
+  KEY `assoc_users_id` (`assoc_users_id`),
+  KEY `assoc_images_id` (`assoc_images_id`),
+  CONSTRAINT `Smala_assoc_users_images_ibfk_1` FOREIGN KEY (`assoc_users_id`) REFERENCES `Smala_users` (`user_id`),
+  CONSTRAINT `Smala_assoc_users_images_ibfk_2` FOREIGN KEY (`assoc_images_id`) REFERENCES `Smala_images` (`image_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- 2021-03-01 08:59:28
